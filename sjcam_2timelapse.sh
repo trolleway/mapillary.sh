fe761d56c88c64b9e6a56e1f46dd1be869a50b14
Artem Svetlov <trolleway@yandex.ru>
2020-06-18 22:48:31 +0300

    Update sjcam_2timelapse.sh

--- sjcam_2timelapse.sh
+++ sjcam_2timelapse.sh
@@ -4,7 +4,7 @@
 video_dir=$(realpath $1)
 
 mkdir $video_dir/frames
-for video in $path/*.MP4
+for video in $video_dir/*.MP4
 do
     duration_frames=$(ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1  $video)
     echo $duration_frames
