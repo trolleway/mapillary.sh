57d1573320ac5a79219487a9aa039ff8c9086715
Artem Svetlov <trolleway@yandex.ru>
2020-06-18 23:34:43 +0300

    Update sjcam_2timelapse.sh

--- sjcam_2timelapse.sh
+++ sjcam_2timelapse.sh
@@ -6,10 +6,11 @@ video_dir=$(realpath $1)
 mkdir $video_dir/frames
 for video in $video_dir/*.MP4
 do
+    video_noext="${video##*.}"
     duration_frames=$(ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1  $video)
     echo $duration_frames
     #
-    seq  0 $duration_frames | parallel --bar  ffmpeg -y -loglevel panic -vf select="between(n\,{}\,{}+1),setpts=PTS-STARTPTS" -i $1   -frames:v 1 $video_dir/frames/$video.{}.JPG 
+    seq  0 $duration_frames | parallel --bar  ffmpeg -y -loglevel panic -"select=gte(n\,{})" -i $1   -frames:v 1 $video_dir/frames/$video_noext.{}.PNG 
 done
 
 #rm -rf $video_dir/frames
