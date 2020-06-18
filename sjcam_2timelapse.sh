77f1842d305fbd4a2ab8971f445766fc8304c90d
Artem Svetlov <trolleway@yandex.ru>
2020-06-18 22:45:07 +0300

    Update sjcam_2timelapse.sh

--- sjcam_2timelapse.sh
+++ sjcam_2timelapse.sh
@@ -3,9 +3,13 @@
 # $1 - path to MP4 folder 
 path=$(realpath $1)
 
+mkdir $video_dir/frames
 for video in $path/*.MP4
 do
-    duration_frames=$(ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of default=nokey=1:noprint_wrappers=1 $video)
+    duration_frames=$(ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1  $video)
     echo $duration_frames
-    #-vf select="between(n\,200\,300),setpts=PTS-STARTPTS"
+    #
+    seq  0 $duration_frames | parallel --bar  ffmpeg -y -loglevel panic -vf select="between(n\,{}\,{}+1),setpts=PTS-STARTPTS" -i $1   -frames:v 1 $video_dir/frames/$video.{}.JPG {} 
 done
+
+#rm -rf $video_dir/frames
