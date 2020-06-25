677a66bfc32d41a94b58639ca5625542155f4caf
trolleway <trolleway@yandex.ru>
2020-06-25 22:25:45 +0300

    upd mijia360

--- mijia360.sh
+++ mijia360.sh
@@ -2,29 +2,35 @@
 
 # $1 - path to folder
 # $2 - angle 
-
-# RUN apt-cache policy tzdata
-# RUN apt-get install except
-
-usage() { printf "Upload Xiaomi Mijia 360 georefrenced photos to Mapillary \n Usage: $0 [-u <mapillary username>] [-p <password>] [-a <offset angle>] [-s <source folder>]" 1>&2; exit 1; }
+# $3 - mapillary username
 
 
 
-USERNAME="trolleway"
-ANGLE="180"
+#If some files has gps tags, sone not has
 
-#search first gpx file in folder
-gpx=$(find $1 -name "*.gpx" |  head -n 1)
-#offset_time=$((3*3600))
-#echo $offset_time
+#drop csv header, calc lines with empty gps tags (looks like ,,)
+nogps_count=$(exiftool -q -q  -csv  -gpslatitude -gpslongitude  $1 | tail -n +2 | grep ',,' |   wc -l)
 
-#i can't fighred how to deal with timezones in mapillary_tools geotag_source, but exiftool seems work
-echo 'geotag'
-#find $1 -name "*.JPG" | parallel --bar exiftool -q -q -overwrite_original -geotag $gpx  {}
+echo nogps_count = $nogps_count
+#need spaces around brackets
+if [ "$nogps_count" -gt 0 ]; then
+	echo 'There are some images withouth GPS positon in input folder. To not mistake folder, Geotag it in JOSM or delete';
+	echo $nogps_list;
+	exit 1;
+fi
 
-exiftool -progress -q -q -overwrite_original -geotag $gpx $1
+#If none fails has gps tags, this columns will not get into CSV. Check CSV header
+nogps_count=$(exiftool  -q -q  -csv  -gpslatitude -gpslongitude  $1 | grep 'GPSLongitude' | wc -l)
 
-mapillary_tools process --advanced --import_path "$1" --user_name $USERNAME --cutoff_distance 100 --cutoff_time 60 \
+#need spaces around brackets
+if [ "$nogps_count" -eq 0 ]; then
+	echo 'There are all images withouth GPS positon in input folder. Geotag it in JOSM';
+
+	exit 1;
+fi
+
+
+mapillary_tools process --advanced --import_path "$1" --user_name $3 --cutoff_distance 100 --cutoff_time 60 \
 --interpolate_directions --offset_angle $2 --rerun --overwrite_EXIF_direction_tag 2> /dev/null  
 
 mapillary_tools upload --import_path "$1" --skip_subfolders --number_threads 4 --max_attempts 50 --advanced
