552b410512c75348e4031bddf90670c60af92705
Artem Svetlov <trolleway@yandex.ru>
2020-07-28 01:35:46 +0300

    Update mijia360_geosync.sh

--- mijia360_geosync.sh
+++ mijia360_geosync.sh
@@ -3,5 +3,11 @@
 # geosync photos. Mijia360 do not store timezone, so manual tiezone shift performing here
 #$1 - path to Mijia360 photos taken in UTM+3 zone
 
-
+gpx=$(find  $1/*.gpx)
+if [ -z "$gpx" ]
+then
+      echo "not foud .gpx file at $1"
+      exit 1
+fi
+      
 exiftool -overwrite_original -P -geotag=$gpx '-Geotime<${DateTimeOriginal}+03:00' $1
