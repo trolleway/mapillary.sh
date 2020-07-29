68171a1d11418ef4a5eaf8264d4ca198e392fb5f
Artem Svetlov <trolleway@yandex.ru>
2020-07-29 23:07:57 +0300

    Update mijia360_geosync.sh

--- mijia360_geosync.sh
+++ mijia360_geosync.sh
@@ -15,4 +15,4 @@ fi
 #total=${ls -1q $1/*.JPG  | wc -l}
 #i=0
 
-exiftool -overwrite_original -q -q -P -progress:Geotagging -geotag=$gpx '-Geotime<${DateTimeOriginal}+03:00' $1
+exiftool -overwrite_original -q -P -progress:Geotagging -geotag=$gpx '-Geotime<${DateTimeOriginal}+03:00' $1
