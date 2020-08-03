26f333ce7bd8c0584186923f6285c47dd48772df
Artem Svetlov <trolleway@yandex.ru>
2020-08-03 16:02:43 +0300

    Update interface.sh

--- interface.sh
+++ interface.sh
@@ -126,7 +126,7 @@ read -n 1 -s -r -p "Press any key to continue"
 
 while true
 do
-DIALOG=${DIALOG=whiptail}
+DIALOG=${DIALOG=dialog}
 tempfile=`mktemp 2>/dev/null` || tempfile=/tmp/test$$
 trap "rm -f $tempfile" 0 1 2 5 15
 
