815f1945d924d34678879e98688224719c747933
Artem Svetlov <trolleway@yandex.ru>
2021-01-05 12:42:55 +0300

    Update and rename mijia360.sh to process_and_upload.sh

--- process_and_upload.sh
+++ process_and_upload.sh
@@ -30,14 +30,9 @@ if [ "$nogps_count" -eq 0 ]; then
 fi
 
 
-mapillary_tools video_process  --video_import_path "$1" \
---user_name $3 --advanced --geotag_source "gopro_videos" --geotag_source_path "$1" \
---interpolate_directions --video_sample_interval 0.5 --interpolate_directions --offset_angle $2 --overwrite_EXIF_gps_tag  2> /dev/null 
+mapillary_tools process --advanced --import_path "$1" --user_name $3 --cutoff_distance 100 --cutoff_time 60 \
+--interpolate_directions --offset_angle $2 --rerun --overwrite_EXIF_direction_tag 2> /dev/null  
 
-#---------------
-#mapillary_tools process --advanced --import_path "$1" --user_name $3 --cutoff_distance 100 --cutoff_time 60 \
-#--interpolate_directions --offset_angle $2 --rerun --overwrite_EXIF_direction_tag 2> /dev/null  
-#
-#mapillary_tools upload --import_path "$1" --skip_subfolders --number_threads 4 --max_attempts 50 --advanced
-echo "End" 
+mapillary_tools upload --import_path "$1" --skip_subfolders --number_threads 4 --max_attempts 50 --advanced
+echo "End"  
   
