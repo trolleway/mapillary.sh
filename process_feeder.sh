e1ca5d5d039f3e917169ce48efce4858414a1bf0
Artem Svetlov <trolleway@yandex.ru>
2021-06-13 02:18:08 +0300

    Update process_feeder.sh

--- process_feeder.sh
+++ process_feeder.sh
@@ -4,16 +4,16 @@
 path="/data/mapillary.sh/feeder"
 
 mapillary_tools process_and_upload --advanced --import_path "$path/0" --user_name trolleway  \
---interpolate_directions --offset_angle 0  --overwrite_EXIF_direction_tag --keep-dublicates
+--interpolate_directions --offset_angle 0  --overwrite_EXIF_direction_tag --keep-duplicates
 mapillary_tools process_and_upload --advanced --import_path "$path/90" --user_name trolleway  \
---interpolate_directions --offset_angle 90  --overwrite_EXIF_direction_tag  --keep-dublicates
+--interpolate_directions --offset_angle 90  --overwrite_EXIF_direction_tag  --keep-duplicates
 mapillary_tools process_and_upload --advanced --import_path "$path/180" --user_name trolleway  \
---interpolate_directions --offset_angle 180  --overwrite_EXIF_direction_tag --keep-dublicates
+--interpolate_directions --offset_angle 180  --overwrite_EXIF_direction_tag --keep-duplicates
 mapillary_tools process_and_upload --advanced --import_path "$path/270" --user_name trolleway  \
---interpolate_directions --offset_angle 270  --overwrite_EXIF_direction_tag  --keep-dublicates
+--interpolate_directions --offset_angle 270  --overwrite_EXIF_direction_tag  --keep-duplicates
 
 mapillary_tools process_and_upload --advanced --import_path "$path/keep_direction" --user_name trolleway   \
-  --keep-dublicates
+  --keep-duplicates
 
 
 echo "End"  
