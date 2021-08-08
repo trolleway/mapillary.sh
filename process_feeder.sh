7b29592a466cfcfbd5dd6b00ef91f54d054c96ff
Artem Svetlov <trolleway@yandex.ru>
2021-08-09 00:30:30 +0300

    typo

--- process_feeder.sh
+++ process_feeder.sh
@@ -15,7 +15,7 @@ mapillary_tools process_and_upload --advanced --import_path "$path/270" --user_n
 mapillary_tools process_and_upload --advanced --import_path "$path/315" --user_name trolleway  \
 --interpolate_directions --offset_angle 315  --overwrite_EXIF_direction_tag  --keep_duplicates
 
-mapillary_tools process_and_upload --advanced --import_path "$path/315" --user_name trolleway  \
+mapillary_tools process_and_upload --advanced --import_path "$path/355" --user_name trolleway  \
 --interpolate_directions --offset_angle 355  --overwrite_EXIF_direction_tag  --keep_duplicates
 
 mapillary_tools process_and_upload --advanced --import_path "$path/keep_direction" --user_name trolleway   \
