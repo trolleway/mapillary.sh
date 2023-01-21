6d2b57f126900d2bd967e5bc79ff904b15f4c36f
Artem Svetlov <trolleway@yandex.ru>
2023-01-22 00:23:14 +0300

    Update process_and_upload.sh

--- process_and_upload.sh
+++ process_and_upload.sh
@@ -30,8 +30,8 @@ if [ "$nogps_count" -eq 0 ]; then
 fi
 
 
-mapillary_tools process --advanced --import_path "$1" --user_name $3 --cutoff_distance 100 --cutoff_time 60 \
---interpolate_directions --offset_angle $2 --rerun --overwrite_EXIF_direction_tag 2> /dev/null  
+mapillary_tools process   --user_name $3 --cutoff_distance 100 --cutoff_time 60 \
+--interpolate_directions --offset_angle $2 --rerun --overwrite_EXIF_direction_tag "$1" 2> /dev/null  
 
 mapillary_tools upload --import_path "$1" --skip_subfolders --number_threads 4 --max_attempts 50 --advanced
 echo "End"  
